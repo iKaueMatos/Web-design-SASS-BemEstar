c0d38536bbc016594714a758465a097ffe4e1c46
ikauematos <kaue178hotimail@gmail.com>
2023-01-01 21:15:22 -0300

    Em fase de aplicação da responsividade

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+const small = window.matchMedia('(max-width:600px)')
+const small_tablet = window.matchMedia('(max-width:700px)')
+const small_desktop = window.matchMedia('(max-width:1440px)')
+
+
+
+if (small){
+    console.log("Uusuario Mobile")
+}else if(small_tablet){
+    console.log("Usuario Tablet")
+}else if(small_desktop){
+    console.log("Usuario de Desktop")
+}
+
+
+
+
+
+
+//Event de clique
 function hadleKeyboard(event){
     if(event.key === 'f11'){
         document.body.classList.toggle('fullscreen')
